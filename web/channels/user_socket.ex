d870b3532c695e0ad356550913c6ab0402d03ae3
Krzysztof Krzeszowski <krzysztof.krzeszowski@gmail.com>
2017-06-20 09:48:02 +0200

    deployment

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule Pxblog.UserSocket do
   # channel "room:*", Pxblog.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
