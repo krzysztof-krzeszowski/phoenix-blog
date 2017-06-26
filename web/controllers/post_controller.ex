7de2e7c7f64f755539592e27c32744d1b361be40
Krzysztof Krzeszowski <krzysztof.krzeszowski@gmail.com>
2017-06-26 21:46:58 +0200

    finished part 8, not including test

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -6,6 +6,7 @@ defmodule Pxblog.PostController do
 
   plug :assign_user
   plug :authorize_user when action in [:new, :create, :update, :edit, :delete]
+  plug :set_authorization_flag
 
   def index(conn, _params) do
     posts = Repo.all(assoc(conn.assigns[:user], :posts))
@@ -97,8 +98,7 @@ defmodule Pxblog.PostController do
   end
 
   defp authorize_user(conn, _opts) do
-    user = get_session(conn, :current_user)
-    if user && (Integer.to_string(user.id) == conn.params["user_id"] || RoleChecker.is_admin?(user)) do
+    if is_authorized_user?(conn) do
       conn
     else
       conn
@@ -107,4 +107,14 @@ defmodule Pxblog.PostController do
       |> halt()
     end
   end
+
+  defp is_authorized_user?(conn) do
+    user = get_session(conn, :current_user)
+    (user && (Integer.to_string(user.id) == conn.params["user_id"] || RoleChecker.is_admin?(user)))
+  end
+
+  def set_authorization_flag(conn, _opts) do
+    assign(conn, :author_or_admin, is_authorized_user?(conn))
+  end
+  
 end
